bbf900356b553af1af24bc1838a8c5403efb29ae
Oguz Vuruskaner <ovuruska@outlook.com>
2020-12-26 17:29:46 +0300

    Fixed padding error.

--- eyecu_good_guys/layers/Inception.py
+++ eyecu_good_guys/layers/Inception.py
@@ -10,11 +10,11 @@ class Inception(N.Module):
         self.x1 = N.Conv2d(in_filters,out_filters//4,1,2)
         self.x2 = N.Sequential(
             N.Conv2d(in_filters, in_filters, 1),
-            N.Conv2d(in_filters, out_filters // 4, 3, 2),
+            N.Conv2d(in_filters, out_filters // 4, 3, 2,1),
         )
         self.x3 = N.Sequential(
             N.Conv2d(in_filters, in_filters, 1),
-            N.Conv2d(in_filters, out_filters // 4, 5, 2),
+            N.Conv2d(in_filters, out_filters // 4, 5, 2,2),
         )
         self.x4 = N.MaxPool2d(3,2,1)
 
@@ -22,8 +22,8 @@ class Inception(N.Module):
     def forward(self,x):
 
         x1 = self.x1(x)
-        x2 = self.x1(x)
-        x3 = self.x1(x)
-        x4 = self.x1(x)
+        x2 = self.x2(x)
+        x3 = self.x3(x)
+        x4 = self.x4(x)
 
         return torch.cat([x1,x2,x3,x4],dim=1)
