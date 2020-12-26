e04222def40e4c6f91ebb118fe72ced8493a25d4
Oguz Vuruskaner <ovuruska@outlook.com>
2020-12-26 17:29:57 +0300

    Added funneled inception layer.

---
+++ eyecu_good_guys/layers/InceptionMerge.py
@@ -0,0 +1,30 @@
+import torch.nn as N
+from .SpatialMaxPooling import SpatialMaxPooling
+
+class InceptionMerge(N.Module):
+
+    def __init__(self, in_filters):
+
+        super().__init__()
+        self.x1 = N.Conv2d(in_filters,1,1,2)
+        self.x2 = N.Sequential(
+            N.Conv2d(in_filters, in_filters, 1),
+            N.Conv2d(in_filters, 1, 3, 2,1),
+        )
+        self.x3 = N.Sequential(
+            N.Conv2d(in_filters, in_filters, 1),
+            N.Conv2d(in_filters, 1, 5, 2,2),
+        )
+        self.x4 = SpatialMaxPooling()
+        
+
+
+    def forward(self,x):
+
+        x1 = self.x1(x)
+        x2 = self.x2(x)
+        x3 = self.x3(x)
+        x4 = self.x4(x)
+
+        return x1+x2+x3+x4
+        # return torch.cat([x1,x2,x3,x4],dim=1)
--- eyecu_good_guys/layers/__init__.py
+++ eyecu_good_guys/layers/__init__.py
@@ -1,8 +1,10 @@
 from .GlobalAveragePooling1d import GlobalAveragePooling1d
 from .GlobalAveragePooling2d import GlobalAveragePooling2d
 from .GlobalAveragePooling3d import GlobalAveragePooling3d
+from .SpatialMaxPooling import SpatialMaxPooling
 
 from .Inception import Inception
+from .InceptionMerge import InceptionMerge
 from .MinibatchDiscrimination import MinibatchDiscrimination
 
 from .Reshape import Reshape
