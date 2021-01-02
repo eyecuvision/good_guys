d08e0c6a798ef43bc3b7daee942708079e149dcb
Oguz Vuruskaner <ovuruska@outlook.com>
2021-01-02 14:15:27 +0300

    Updated tests of BiFPN.

--- tests/test_BidirectionalFeaturePyramidalNetwork.py
+++ tests/test_BidirectionalFeaturePyramidalNetwork.py
@@ -3,36 +3,36 @@ from good_guys.layers import BiFPN
 from itertools import product
 import torch
 
+
 class TestBiFPN(TestCase):
-    FILTERS = [1,16,63]
-    BATCH_SIZES = [1,16,63]
-    LOG_2_VALUES = [4,5,6]
+    BATCH_SIZES = [1, 16]
+    FILTERS = [16, 32, 64, 128, 128]
 
     def test_forward_divisible_by_2(self):
 
         # Bigger images have smaller indices
 
+        for batch_size in self.BATCH_SIZES:
 
-        for filters,batch_size,log_2 in product(self.FILTERS,self.BATCH_SIZES,self.LOG_2_VALUES):
-
-            batch_size = 1
-            layer = BiFPN(filters,log_2)
-            image = torch.randn((batch_size,filters,2**log_2,2**log_2))
+            root_dim = 64
+            dim = root_dim
 
             inputs = []
-            for i in range(log_2):
+
+            for filter in self.FILTERS:
+                image = torch.randn((batch_size, filter, dim, dim))
                 inputs.append(image)
-                image = image[:,:,::2,::2]
+                dim = dim // 2
 
+            module = BiFPN(self.FILTERS)
+            result =  module(inputs)
 
-            result = layer(inputs)
-            self.assertEqual(len(result),log_2)
+            for inp,result in zip(inputs,result):
 
-            for ind,feature in enumerate(result):
-                self.assertEqual(feature.shape[0],batch_size)
-                self.assertEqual(feature.shape[1],filters)
-                self.assertEqual(feature.shape[2],2**(log_2-ind))
-                self.assertEqual(feature.shape[3],2**(log_2-ind))
+                expected_size = list(inp.size())
+                result_size   = list(result.size())
+
+                self.assertEqual(expected_size,result_size)
 
 
     def test_forward_not_divisible_by_2(self):
