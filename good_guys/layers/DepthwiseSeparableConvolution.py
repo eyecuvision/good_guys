6f986449a1703e017ad6d7fe0f0f3918ab897ee2
Oguz Vuruskaner <ovuruska@outlook.com>
2021-01-02 13:08:22 +0300

    Fixed padding value for even numbers.

--- good_guys/layers/DepthwiseSeparableConvolution.py
+++ good_guys/layers/DepthwiseSeparableConvolution.py
@@ -5,9 +5,14 @@ class DepthwiseSeparableConvolution(N.Module):
 
     def __init__(self,filters : int,kernel_size:int):
         super().__init__()
+
+        self.padding = (kernel_size-1)//2
+
         self.l = N.Sequential(
-            N.Conv2d(filters,filters,(kernel_size,1),1,(kernel_size//2,0),groups=filters),
-            N.Conv2d(filters,filters,(1,kernel_size),1,(0,kernel_size//2),groups=filters),
+            N.Conv2d(filters,filters,(kernel_size,1),1,(self.padding,0),groups=filters),
+            N.BatchNorm2d(filters),
+            N.LeakyReLU(inplace=True),
+            N.Conv2d(filters,filters,(1,kernel_size),1,(0,self.padding),groups=filters),
             N.BatchNorm2d(filters),
             N.LeakyReLU(inplace=True),
             N.Conv2d(filters,filters,1),
